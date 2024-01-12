57abb578b7152b62a942dce03d105d7fb5fdedba
Ghassan Baltaji <78371137+ghassanbaltaji@users.noreply.github.com>
2024-01-12 15:48:24 +0100

    Update select.sql

--- select.sql
+++ select.sql
@@ -21,3 +21,19 @@ SELECT
     *
 FROM
     departments;
+
+-- Select specific name
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Denis';
+
+-- Select all people from the “employees” table whose first name is “Elvis”
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Elvis';
