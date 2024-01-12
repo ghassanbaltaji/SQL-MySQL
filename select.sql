1547c1c078b8a9f51756eb9b0027195e6c426961
Ghassan Baltaji <78371137+ghassanbaltaji@users.noreply.github.com>
2024-01-12 15:30:10 +0100

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,22 +1,22 @@
-'Select specific column'
+-- Select specific column
 SELECT 
     first_name, last_name
 FROM
     employees;
  
-'Select the entire data of "employees" database'
+-- Select the entire data of "employees" table
 SELECT 
     *
 FROM
     employees;
     
-' Select the information from the “dept_no” column of the “departments” table'    
+-- Select the information from the “dept_no” column of the “departments” table   
 SELECT 
     dept_no
 FROM
     departments;
 
-'Select all data from the “departments” table' 
+-- Select all data from the “departments” table 
 SELECT 
     *
 FROM
