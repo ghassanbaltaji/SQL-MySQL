e4ae9e4efb44ed851680668a2496a40eaf0788af
Ghassan Baltaji <78371137+ghassanbaltaji@users.noreply.github.com>
2024-01-12 15:23:33 +0100

    Create select.sql

---
+++ select.sql
@@ -0,0 +1,23 @@
+'Select specific column'
+SELECT 
+    first_name, last_name
+FROM
+    employees;
+ 
+'Select the entire data of "employees" database'
+SELECT 
+    *
+FROM
+    employees;
+    
+' Select the information from the “dept_no” column of the “departments” table'    
+SELECT 
+    dept_no
+FROM
+    departments;
+
+'Select all data from the “departments” table' 
+SELECT 
+    *
+FROM
+    departments;
