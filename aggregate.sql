d7bde27d98a9509b3ec43bc9190ffc6a8745e3fe
Ghassan Baltaji <78371137+ghassanbaltaji@users.noreply.github.com>
2024-01-15 19:24:21 +0100

    Update aggregate.sql

--- aggregate.sql
+++ aggregate.sql
@@ -36,6 +36,15 @@ WHERE
 SELECT
     ROUND(AVG(salary), 2)
 FROM
-    salaries
+    salaries    
 WHERE
     from_date > '1997-01-01';
+
+-- Select the department number and name from the ‘departments_dup’ table and add a third column where you name the department number (‘dept_no’) as ‘dept_info’. If ‘dept_no’ does not have a value, use ‘dept_name’.
+SELECT
+    dept_no,
+    dept_name,
+    COALESCE(dept_no, dept_name) AS dept_info
+FROM
+    departments_dup
+ORDER BY dept_no ASC;
